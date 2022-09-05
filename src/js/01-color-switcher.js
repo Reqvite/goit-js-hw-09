0455596d2a1f120a24a1679177ef07ea83113b6b
Reqvite <reqvite2233@gmail.com>
2022-09-05 16:13:34 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]')
+}
 
+refs.btnStart.addEventListener('click', turnOnInterval)
+refs.btnStop.addEventListener('click', turnOffInterval)
+
+function turnOnInterval() {
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.btnStart.setAttribute('disabled', 0);
+}
+function turnOffInterval() {
+    clearInterval(intervalId);
+    refs.btnStart.removeAttribute('disabled');
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
