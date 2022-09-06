e170f2af2c2e290b1aa997a66c72e32a53454067
Reqvite <reqvite2233@gmail.com>
2022-09-06 17:50:12 +0300

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,12 +4,13 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const refs = {
     btnStart: document.querySelector('[data-start]'),
-    input: document.querySelector('input')
+    input: document.querySelector('input'),
+    timerInterface: document.querySelectorAll('.value')
 }
 
-let timerId = null
+let timerId = null;
 refs.btnStart.disabled = true;
-let maxTimerTime = 0
+let maxTimerTime = 0;
 
 const options = {
   enableTime: true,
@@ -25,14 +26,14 @@ const options = {
         } else {
             refs.btnStart.disabled = false;
             maxTimerTime = timerValue;
-
         }
   },
 };
 flatpickr(refs.input, options);
 
 refs.btnStart.addEventListener('click', () => {
-    turnOnTimer(maxTimerTime)
+    Notiflix.Notify.success('Timer started');
+    turnOnTimer(maxTimerTime);
     refs.btnStart.disabled = true;
 })
 
@@ -42,25 +43,32 @@ function turnOnTimer(maxTimerTime) {
         const deltaTime = new Date() - startTime;
         currenntTime = Math.abs(maxTimerTime) - deltaTime;
         const { days, hours, minutes, seconds } = convertMs(currenntTime); 
-        // Math.abs(maxTimerTime)
-        console.log({ days, hours, minutes, seconds });
-        if (currenntTime <= 0) {
-             clearInterval(timerId)
+        updateTimer(convertMs(currenntTime))
+        if (seconds === '00') {
+            clearInterval(timerId);
             Notiflix.Notify.info('Timer is over'); 
         }
     }, 1000)   
 }
 
+function updateTimer({ days, hours, minutes, seconds }) {
+    refs.timerInterface[0].textContent = days;
+    refs.timerInterface[1].textContent = hours;
+    refs.timerInterface[2].textContent = minutes;
+    refs.timerInterface[3].textContent = seconds;
+}
 
+function addLeadingZero(maxTimerTime){
+    return String(maxTimerTime).padStart(2, '0');
+}
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
   return { days, hours, minutes, seconds };
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,57 @@
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('form')
+
+const formData = {}
+let intervalId = null;
+let position = 0;
+let TIMER = 0;
+let delay = 0;
+
+
+formRef.addEventListener('input', getData)
+formRef.addEventListener('submit', (e) => {
+  e.preventDefault() 
+  turnOnCounter()
+})
+
+function getData(e) {
+  formData[e.target.name] = e.target.value
+  delay = Number(formData.delay)
+  TIMER = formData.step
+}
+
+function turnOnCounter() {
+  setTimeout(() => {
+    position += 1;
+    createPromise(position, delay)
+    turnOnInterval()
+  }, formData.delay);
+}
+
+function turnOnInterval() {
+  intervalId = setInterval(() => {
+    position += 1;
+    delay += Number(TIMER)
+    console.log(delay);
+     createPromise(position, delay)
+    if (String(position) === formData.amount) {
+      clearInterval(intervalId)
+    }
+  }, TIMER)
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+ return new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve({ position, delay })
   } else {
-    // Reject
-  }
+        reject({ position, delay })
+    }
+  })
+  
 }
+
+
+
